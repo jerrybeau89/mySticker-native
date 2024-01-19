9d8d4f5b3de14cbbc44dc926ac7d749e2c540c52
Jerry Beau Baggett <117130414+jerrybeau89@users.noreply.github.com>
2024-01-19 10:00:50 -0800

    added button and image viewer components. Linked them to app.js

---
+++ MySticker/App.js
@@ -0,0 +1,66 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+
+const PlaceholderImage = require("./assets/background-image.jpeg");
+
+export default function App() {
+  console.log("App executed");
+
+  const [selectedImage, setSelectedImage] = useState(null);
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Text style={styles.text}>ReHomeIt World!</Text>
+        <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button
+          theme="primary"
+          label="Choose a photo"
+          onPress={pickImageAsync}
+        />
+        <Button label="Use this photo" />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 40,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+  },
+});
